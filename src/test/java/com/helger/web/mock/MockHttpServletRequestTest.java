14d4e516a81dd156d93cc845d360d9ee930dc327
phax <philip@helger.com>
2015-07-01 22:57:09 +0200

    Fixed missing shutdown

--- src/test/java/com/helger/web/mock/MockHttpServletRequestTest.java
+++ src/test/java/com/helger/web/mock/MockHttpServletRequestTest.java
@@ -19,15 +19,13 @@ package com.helger.web.mock;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
-import javax.servlet.ServletContext;
-
 import org.junit.Test;
 
 import com.helger.web.http.EHTTPMethod;
 
 /**
  * Test class for class {@link MockHttpServletRequest}.
- * 
+ *
  * @author Philip Helger
  */
 public final class MockHttpServletRequestTest
@@ -36,27 +34,34 @@ public final class MockHttpServletRequestTest
   public void testSetPathsFromRequestURI ()
   {
     final String sContextPath = "/ctx";
-    final ServletContext aSC = new MockServletContext (sContextPath);
-    final MockHttpServletRequest c = new MockHttpServletRequest (aSC, EHTTPMethod.GET);
+    final MockServletContext aSC = new MockServletContext (sContextPath);
+    try
+    {
+      final MockHttpServletRequest c = new MockHttpServletRequest (aSC, EHTTPMethod.GET);
 
-    c.setAllPaths (sContextPath + "/servlet?x=y");
-    assertNull (c.getScheme ());
-    assertNull (c.getServerName ());
-    assertEquals (-1, c.getServerPort ());
-    assertEquals (sContextPath, c.getContextPath ());
-    assertEquals ("/servlet", c.getServletPath ());
-    assertEquals ("", c.getPathInfo ());
-    assertEquals (sContextPath + "/servlet", c.getRequestURI ());
-    assertEquals ("x=y", c.getQueryString ());
+      c.setAllPaths (sContextPath + "/servlet?x=y");
+      assertNull (c.getScheme ());
+      assertNull (c.getServerName ());
+      assertEquals (-1, c.getServerPort ());
+      assertEquals (sContextPath, c.getContextPath ());
+      assertEquals ("/servlet", c.getServletPath ());
+      assertEquals ("", c.getPathInfo ());
+      assertEquals (sContextPath + "/servlet", c.getRequestURI ());
+      assertEquals ("x=y", c.getQueryString ());
 
-    c.setAllPaths (sContextPath + "/servlet/path/in/servlet#anchor");
-    assertNull (c.getScheme ());
-    assertNull (c.getServerName ());
-    assertEquals (-1, c.getServerPort ());
-    assertEquals (sContextPath, c.getContextPath ());
-    assertEquals ("/servlet", c.getServletPath ());
-    assertEquals ("/path/in/servlet", c.getPathInfo ());
-    assertEquals (sContextPath + "/servlet/path/in/servlet", c.getRequestURI ());
-    assertNull (c.getQueryString ());
+      c.setAllPaths (sContextPath + "/servlet/path/in/servlet#anchor");
+      assertNull (c.getScheme ());
+      assertNull (c.getServerName ());
+      assertEquals (-1, c.getServerPort ());
+      assertEquals (sContextPath, c.getContextPath ());
+      assertEquals ("/servlet", c.getServletPath ());
+      assertEquals ("/path/in/servlet", c.getPathInfo ());
+      assertEquals (sContextPath + "/servlet/path/in/servlet", c.getRequestURI ());
+      assertNull (c.getQueryString ());
+    }
+    finally
+    {
+      aSC.invalidate ();
+    }
   }
 }

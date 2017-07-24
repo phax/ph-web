6cbd3a2db8c436f7a455c5d1c43b245c29d70606
Philip Helger <philip@helger.com>
2017-07-24 20:25:28 +0200

    Added comment for Chrome 59

--- ph-http/src/main/java/com/helger/http/EHTTPReferrerPolicy.java
+++ ph-http/src/main/java/com/helger/http/EHTTPReferrerPolicy.java
@@ -5,18 +5,21 @@ import javax.annotation.Nonnull;
 /**
  * HTTP response header "Referrer-Policy" values. See
  * https://scotthelme.co.uk/a-new-security-header-referrer-policy/
- * 
+ *
  * @author Philip Helger
  */
 public enum EHTTPReferrerPolicy
 {
   NONE (""),
   NO_REFERRER ("no-referrer"),
-  NO_REFERRER_WHEN_DOWNGRAD ("no-referrer-when-downgrade"),
+  NO_REFERRER_WHEN_DOWNGRADE ("no-referrer-when-downgrade"),
+  // Not supported in Chrome 59
   SAME_ORIGIN ("same-origin"),
   ORIGIN ("origin"),
+  // Not supported in Chrome 59
   STRICT_ORIGIN ("strict-origin"),
   ORIGIN_WHEN_CROSS_ORIGIN ("origin-when-cross-origin"),
+  // Not supported in Chrome 59
   STRICT_ORIGIN_WHEN_CROSS_ORIGIN ("strict-origin-when-cross-origin"),
   UNSAFE_URL ("unsafe-url");
 

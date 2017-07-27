1a16983fce182411712eb972d0e8769c13c662b2
Philip Helger <philip@helger.com>
2017-07-28 00:40:56 +0200

    Extended API

--- ph-web/src/main/java/com/helger/web/scope/IRequestParamContainer.java
+++ ph-web/src/main/java/com/helger/web/scope/IRequestParamContainer.java
@@ -5,21 +5,46 @@ import java.util.Map;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
+import com.helger.commons.ValueEnforcer;
+import com.helger.commons.annotation.Nonempty;
 import com.helger.commons.collection.ArrayHelper;
 import com.helger.commons.collection.attr.IMutableAttributeContainerAny;
 import com.helger.commons.collection.impl.CommonsArrayList;
 import com.helger.commons.collection.impl.CommonsHashMap;
 import com.helger.commons.collection.impl.ICommonsList;
 import com.helger.commons.collection.impl.ICommonsMap;
+import com.helger.commons.string.StringHelper;
 import com.helger.web.fileupload.IFileItem;
 
 /**
- * A special request parameter map with support for file items etc.
+ * A special request parameter container with support for file items etc.
  *
  * @author Philip Helger
  */
 public interface IRequestParamContainer extends IMutableAttributeContainerAny <String>
 {
+  /**
+   * The prefix to appended to the field name of the checkbox to create the
+   * hidden field.
+   */
+  String DEFAULT_HIDDEN_FIELD_PREFIX = "__";
+
+  /**
+   * Get the name of the automatic hidden field associated with a check-box.
+   *
+   * @param sFieldName
+   *        The name of the check-box.
+   * @return The name of the hidden field associated with the given check-box
+   *         name.
+   */
+  @Nonnull
+  @Nonempty
+  static String getHiddenFieldName (@Nonnull @Nonempty final String sFieldName)
+  {
+    ValueEnforcer.notEmpty (sFieldName, "FieldName");
+    return DEFAULT_HIDDEN_FIELD_PREFIX + sFieldName;
+  }
+
   /**
    * @return A non-<code>null</code> but maybe empty map with all contained
    *         {@link IFileItem} objects from file uploads. The key of the map is
@@ -97,5 +122,36 @@ public interface IRequestParamContainer extends IMutableAttributeContainerAny <S
   default IFileItem getAsFileItem (@Nullable final String sAttrName)
   {
     return getSafeCastedValue (sAttrName, IFileItem.class);
+  }
+
+  /**
+   * Get the value of the checkbox of the request parameter with the given name.
+   *
+   * @param sFieldName
+   *        Request parameter name. May be <code>null</code>.
+   * @param bDefaultValue
+   *        the default value to be returned, if no request attribute is present
+   * @return <code>true</code> if the checkbox is checked, <code>false</code> if
+   *         it is not checked and the default value otherwise.
+   */
+  default boolean isCheckBoxChecked (@Nullable final String sFieldName, final boolean bDefaultValue)
+  {
+    if (StringHelper.hasText (sFieldName))
+    {
+      // Is the checked value present?
+      final String sRequestValue = getAsString (sFieldName);
+      if (sRequestValue != null)
+        return true;
+
+      // Check if the hidden parameter for "checkbox is contained in the
+      // request" is present?
+      // If so it means the checkbox parameter is part of the request, but the
+      // checkbox is not checked
+      if (containsKey (getHiddenFieldName (sFieldName)))
+        return false;
+    }
+
+    // Neither nor - default!
+    return bDefaultValue;
   }
 }

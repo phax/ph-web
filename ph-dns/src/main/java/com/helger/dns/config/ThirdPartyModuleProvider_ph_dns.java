6762caace9adc37e91bbe9212a37b044753cf967
Philip Helger <philip@helger.com>
2021-09-28 12:52:00 +0200

    Updated to dnsjava 3.4.2

--- ph-dns/src/main/java/com/helger/dns/config/ThirdPartyModuleProvider_ph_dns.java
+++ ph-dns/src/main/java/com/helger/dns/config/ThirdPartyModuleProvider_ph_dns.java
@@ -37,7 +37,7 @@ public final class ThirdPartyModuleProvider_ph_dns implements IThirdPartyModuleP
   public static final IThirdPartyModule DNSJAVA = new ThirdPartyModule ("dnsjava",
                                                                         "Brian Wellington",
                                                                         ELicense.BSD,
-                                                                        new Version (3, 4, 1),
+                                                                        new Version (3, 4, 2),
                                                                         "https://github.com/dnsjava/dnsjava");
 
   @Nullable

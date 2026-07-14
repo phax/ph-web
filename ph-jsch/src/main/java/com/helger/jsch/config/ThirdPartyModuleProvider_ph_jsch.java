af5e9b7c43336a0acbcf4b4a63a3c1106af7326f
Philip Helger <philip@helger.com>
2026-07-14 10:34:54 +0200

    Version bump

--- ph-jsch/src/main/java/com/helger/jsch/config/ThirdPartyModuleProvider_ph_jsch.java
+++ ph-jsch/src/main/java/com/helger/jsch/config/ThirdPartyModuleProvider_ph_jsch.java
@@ -36,7 +36,7 @@ public final class ThirdPartyModuleProvider_ph_jsch implements IThirdPartyModule
   public static final IThirdPartyModule JSCH = new ThirdPartyModule ("JSch",
                                                                      "Matthias Wiedemann",
                                                                      ELicense.BSD,
-                                                                     new Version (2, 28, 3),
+                                                                     new Version (2, 28, 4),
                                                                      "https://github.com/mwiede/jsch");
 
   @Nullable
